68ab84ea9b6ebd482113ccfe50717b156a330bf5
Mohammad Waris <31559101+themdwaris@users.noreply.github.com>
2024-08-13 16:53:25 +0530

    Update InputBox.jsx

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -6,7 +6,7 @@ const InputBox = () => {
     const {addTodo}=useToDo()
 
     const add=(e)=>{
-        e.preventDefault()
+        // e.preventDefault()
         if(!todoInput) return
 
         addTodo({todoInput,completed:false})
@@ -14,7 +14,11 @@ const InputBox = () => {
     }
   return (
     <>
-    <input className="w-full p-3 rounded-tl-lg rounded-bl-lg text-balance bg-white/25  text-white text-xl border-none outline-none"type="text" placeholder="Write Todo..." value={todoInput} onChange={(e)=>setToDoInput(e.target.value)}/>
+    <input className="w-full p-3 rounded-tl-lg rounded-bl-lg text-balance bg-white/25  text-white text-xl border-none outline-none"type="text" placeholder="Write Todo..." value={todoInput} onChange={(e)=>setToDoInput(e.target.value)} onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            add();
+          }
+        }}/>
     <button className="px-4 py-[14px] bg-purple-700 text-white text-[16px] cursor-pointer rounded-tr-lg rounded-br-lg -ml-2 font-semibold" onClick={add}>Add</button>
     </>
   )
